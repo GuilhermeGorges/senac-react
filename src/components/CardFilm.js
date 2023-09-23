0860ee5fdbe4e99cde59084af8e36a20bb44fb7a
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2023-09-22 21:18:48 -0300

    state

--- src/components/CardFilm.js
+++ src/components/CardFilm.js
@@ -1,13 +1,30 @@
+"use client"
 
 import { Star } from "lucide-react";
-
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { useState } from "react";
 
 export default function CardFilme({ filme }) {
-  const { titulo, nota, poster } = filme
+  const { titulo, nota, poster } = filme;
+  const [favorito, setFavorito] = useState()
 
+  function favoritar() {
+    setFavorito(!favorito)
+  }
 
   return (
     <div className="flex flex-col items-center w-40 relative">
+      {favorito ?
+        <FavoriteIcon
+          className="text-rose-600 absolute right-1 top-1 cursor-pointer" 
+          onClick={favoritar}
+        /> :
+        <FavoriteBorderIcon 
+          className="absolute right-1 top-1 cursor-pointer"
+          onClick={favoritar}
+          />
+      }
       <img className="rounded" src={poster} alt="poster do filme" />
       <span className="text-center line-clamp-1">
         {titulo}
